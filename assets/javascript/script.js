54def006c5b24c0a111065cb2145312a59fd7e5e
Vitaliy Vorobyev <vvitali26@gmail.com>
2017-11-17 16:52:57 +0000

    added: function for displaying group members v0.1

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,15 @@
-//alert("Hello World!");
+console.log("main script.js connected")
+
+function displayGroupMembers(name) {
+
+    var member = $("<li class='collection-item'>");
+    member.text(name);
+    $("#member-list").append(member);
+}
+
+$(document).ready(function() {
+    //
+    for (var i = 0; i < 10; i++) {
+        displayGroupMembers(i);
+    }
+});
