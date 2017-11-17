23330b646fce44e9060f3a6f9de14b0f4a7ce6d5
Vitaliy Vorobyev <vvitali26@gmail.com>
2017-11-17 17:56:28 +0000

    reading group's members from db and parsing 0.2

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,15 +1,41 @@
 console.log("main script.js connected")
 
-function displayGroupMembers(name) {
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyC0eSKT_b9dNLmeNGJmBvy5XNtmHoQ1jQY",
+    authDomain: "team-project1.firebaseapp.com",
+    databaseURL: "https://team-project1.firebaseio.com",
+    projectId: "team-project1",
+    storageBucket: "team-project1.appspot.com",
+    messagingSenderId: "724719626718"
+};
+/* global firebase */
+firebase.initializeApp(config);
 
-    var member = $("<li class='collection-item'>");
-    member.text(name);
-    $("#member-list").append(member);
-}
+var db = firebase.database();
 
 $(document).ready(function() {
-    //
-    for (var i = 0; i < 10; i++) {
-        displayGroupMembers(i);
-    }
+
+    /* global firebase */
+    displayGroupMembers("First group");
 });
+
+function displayGroupMembers(groupName) {
+
+    var group = db.ref("groups/0/followers");
+    console.log("start: displaying " + groupName)
+    db.ref("groups/0/followers").on("value", function(snapshot) {
+        console.log("Followers:" + snapshot.val());
+    });
+    //.equalTo("123").
+    db.ref("users/").orderByChild("uniqieId").equalTo("123").on("child_added", function(snapshot) {
+        console.log("23:" + snapshot.key);
+    });
+
+    group.on("child_added", function(snapshot) {
+        console.log(snapshot);
+        $("#member-list").append($("<li class='collection-item'>").text(snapshot.val()));
+    });
+    //do not touch please
+    //var member = $("<li class='collection-item'>").text(name);
+}
