7f9d8fe04dfbec99be66a1026068a659bd098191
Vitaliy Vorobyev <vvitali26@gmail.com>
2017-11-20 02:25:32 +0000

    added: displaying user's ideas from DB, redesigned js

--- assets/javascript/mainDashboardScript.js
+++ assets/javascript/mainDashboardScript.js
@@ -1,17 +1,43 @@
-/*global $*/
+/*global firebase $*/
+
+function addNewIdeaChip(ideaName) {
+    var newIdea = $("<div>").addClass("chip close");
+    newIdea.text(ideaName);
+    var close = $("<i>").addClass("material-icons close");
+    close.text("close");
+    newIdea.append(close);
+    $("#yourGiftIdeas").append(newIdea);
+}
 
 $(document).ready(function() {
+    // TODO only for testing, we have to replace this variables by userID after we will get authentication done
+    var userId = 1;
+    var groupId = 1;
 
-    $("#giftIdeaButton").click( function() {
-        
-        $("#yourGiftIdeas").append($("<div>").addClass("chip close"));
-        
-        $(".chip.close:last").text($("#giftIdea").val());
-        $(".chip.close:last").append($("<i>").addClass("material-icons close"));
-        $(".chip.close:last i").text("close");
+    var db = firebase.database();
+    db.ref("/groups/" + groupId + "/followers/" + userId + "/items")
+        .on('child_added', function(snap) {
+            addNewIdeaChip(snap.val());
+        });
 
-        $("#giftIdea").val("");        
+    $("#yourGiftIdeas").append();
+
+
+    $("#giftIdeaButton").click(function() {
+        addNewIdeaChip($("#giftIdea").val());
+
+        // $("#yourGiftIdeas").append($("<div>").addClass("chip close"));
+        // $(".chip.close:last").text($("#giftIdea").val());
+        // $(".chip.close:last").append($("<i>").addClass("material-icons close"));
+        // $(".chip.close:last i").text("close");
+
+        $("#giftIdea").val("");
     });
 
-    
-}); 
+
+
+
+
+
+
+});
