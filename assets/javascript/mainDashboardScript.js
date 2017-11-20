0b48c38c133feb23a353f1a06166e04e422e6c55
Stephen Chapman <schapm16@gmail.com>
2017-11-20 18:31:51 +0000

    Added dropwdown menu.  Enabled Walmart API result carousel to be displayed.

--- assets/javascript/mainDashboardScript.js
+++ assets/javascript/mainDashboardScript.js
@@ -37,4 +37,74 @@ $(document).ready(function() {
 
         $("#giftIdea").val("");
     });
+    
+    
+    
+    // Function to make Walmart API call and Display Results
+    function walmartAPI(searchTerm) {
+        var priceRange = 40;
+            
+        var key = "bznsyj8ykctspk7c3fr4swkz";
+        var url = "https://api.walmartlabs.com/v1/search?";
+        
+        //Walmart Carousel
+            $.ajax({
+                url: url,
+                method: "GET",
+                jsonp: "callback",
+                dataType: "jsonp",
+                data: {
+                    query: searchTerm,
+                    format: "json",
+                    //"facet.range": priceRange, //
+                    apiKey: key
+                },
+            }).done(function(result) {
+                console.log(result);
+                for (var i = 0; i < 10; i++) {
+                    var newImg = $("<img>");
+                    newImg.attr("src", result.items[i].imageEntities[0].mediumImage);
+                    var Paragraph = $("<p>");
+                    Paragraph.text("Item" + i + ": $" + result.items[i].salePrice);
+                    var caruItem = $("<a id=img" + i + ">");
+                    caruItem.attr("class", 'carousel-item center-align');
+                    caruItem.attr("target", "_blank");
+                    caruItem.attr("href", result.items[i].productUrl);
+                    caruItem.append(newImg);
+                    caruItem.append(Paragraph);
+                    $("#productDisplay").append(caruItem);
+                    $("#img" + i).hammer();
+                    $("#img" + i).on("tap", function() {
+                        window.open($(this).attr("href"), '_blank');
+                    });
+                }
+                $('.carousel').carousel();
+            });
+    }
+    
+    
+    
+    // Product Display Carousel on partner gift idea "chip" click
+    
+    var store = "Walmart";
+    var searchTerm;
+    
+    
+    
+    
+    $(document).on("click", "#partnerGiftIdeas .chip", function() {
+        
+        $('.carousel').carousel('destroy');
+        $("#productDisplay").css("display", "block");
+        $("#productDisplay").empty();
+        searchTerm = $(this).text();
+        
+        if (store === "Walmart") {
+            walmartAPI(searchTerm);    
+        }
+        
+        if (store === "Ebay") {
+            ebayAPI(searchTerm);
+        }
+    });
 });
