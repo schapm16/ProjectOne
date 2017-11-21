4107b839123f45e84d2500caf63a0de3181d7e74
71emj <tim.jeng@gmail.com>
2017-11-21 13:21:31 -0500

    shuffling update

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,30 +9,31 @@ $(document).ready(function() {
     db.ref("users/" + auth + "/groups").on("value", function(snapshot) {
         console.log("Groups:" + snapshot.val().split(","));
         snapshot.val().split(",").forEach(function(element) {
-            displayGroup(element, element)
+            displayGroup(element, element);
             displayGroupMembers(element);
+            $("#" + element + ">i").onclick = shuffleMemberList('Razer');
         });
     });
 });
 
-function displayGroup(groupName, groudId) {
-    var group = $("<div class='group-item' id='group-" + groudId + "'>");
-    var form = $("<form class='input-field scale-transition scale-out' id='emailForm" + groudId + ">");
+function displayGroup(groupName, groupId) {
+    var group = $("<div class='group-item' id='group-" + groupId + "'>");
+    var form = $("<form class='input-field scale-transition scale-out' id='emailForm" + groupId + ">");
+
     group.append($("<h3 class='center'>"));
-    group.append($("<h5>").html(" <span id='member-count" + groudId + "'> </span>"));
-    group.append($("<ul class='collection' id='member-list" + groudId + "'>"));
-    group.append("<a class='waves-effect waves-light btn'><i class='material-icons left'>ac_unit</i>Start</a>");
+    group.append($("<h5>").html(" <span id='member-count" + groupId + "'> </span>"));
+    group.append($("<ul class='collection' id='member-list" + groupId + "'>"));
+    group.append("<a class='waves-effect waves-light btn' id='" + groupId + "'><i class='material-icons left'>ac_unit</i>Start</a>");
     group.append("<a class='waves-effect waves-light btn' id='expandEmail0'><i class='material-icons left'>email</i>Add</a>");
     form.append("<input id='inviteEmail' type='email' class='validate' style='width:80%'>");
     form.append("<label for='inviteEmail'>Email</label>");
     form.append("<button id='inviteEmailButton' type='button' class='btn-floating btn-large right'><i class='material-icons'>arrow_forward</i></button>");
     group.append(form);
+
     $(".s6").append(group);
 }
 
 // displayGroup("Group One", 0);
-
-
 
 
 //TODO button click event listener
@@ -61,9 +62,52 @@ function displayGroupMembers(groupId) {
     function updateMemberCount(groupID) {
         $("#member-count" + groupID).text("Secret Santas: " + $("#member-list" + groupID + " li").length);
     }
-
-
-
     //do not touch please
     //var member = $("<li class='collection-item'>").text(name);
+}
+
+
+function shuffleMemberList(groupName) {
+    // Fisher-Yates shuffling algorithm
+
+    const swap = (targetArray, m, i) => {
+        let temp = targetArray[i];
+        targetArray[i] = targetArray[m];
+        targetArray[m] = temp;
+    };
+
+    const arrayForShuffling = new Array();
+
+    for (let i = 0; i < 10; ++i) {
+        arrayForShuffling.push(i);
+    }
+
+    function shuffle(targetArray) {
+        let m = targetArray.length,
+            i;
+        while (m) {
+            i = Math.floor(Math.random() * m--);
+            swap(targetArray, i, m);
+        }
+        return targetArray;
+    }
+
+    // shuffling 
+
+    const ssAppDatabse = firebase.database(ssl);
+
+    ssAppDatabse.ref('/groups/' + groupName + '/followers/').once('value').then(function(snapshot) {
+        const memberList = Object.getOwnPropertyNames(snapshot.val());
+        console.log(memberList);
+
+        const shuffledList = shuffle(memberList);
+        ssAppDatabse.ref('/groups/' + groupName + '/followers/').set(true);
+        shuffledList.forEach(function(elem, index) {
+            if (index === shuffledList.length) {
+                ssAppDatabse.ref('/groups/' + groupName + '/followers/' + elem).set(shuffledList[0]);
+                return;
+            }
+            ssAppDatabse.ref('/groups/' + groupName + '/followers/' + elem).set(shuffledList[index + 1]);
+        });
+    });
 }
